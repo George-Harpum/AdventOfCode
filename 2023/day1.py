6ddbca88ea737ed284ed94d25e1391aa980cbee5
Elusive-Penguin <gesharpum@gmail.com>
2024-01-26 17:26:44 +0000

    day1 solution

--- 2023/day1.py
+++ 2023/day1.py
@@ -6,30 +6,34 @@ Part 1
 PART = 1
 FILE_LOC = "../inputs/day1.txt"
 
-def part1(file_loc = FILE_LOC):
-    
-    def int_in_str(string: str):
-        out = []
-        for char in string:
-            if char.isdigit():
-                out.append(char)
-        return out
+import re
+
+def part1(content):
     digits = []
-    with open(file_loc) as file:
-        for line in file.readlines():
-            res = int_in_str(line)
-            digits += [int(res[0] + res[-1])]
+    pattern = re.compile("[0-9]")
+    for line in content:
+        res = pattern.findall(line)
+        digits += [int(res[0]+res[-1])]
     return sum(digits)
 
-def part2(file_loc = FILE_LOC):
-    raise NotImplementedError
-
+def part2(content):
+    digits = []
+    num_as_word = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    num_dict = dict(zip(num_as_word, [str(x) for x in range(10)]))
+    pattern = re.compile("|".join(num_as_word)+"[0-9]")
+    for line in content:
+        find = pattern.findall(line)
+        res = list(map(lambda x: num_dict.get(x, x), find))
+        digits += [int(res[0]+res[-1])]
+    return sum(digits)
 
 if __name__ == "__main__":
+    if PART not in (1, 2):
+        raise ValueError("Part must be 1 or 2")
+    file = open(FILE_LOC, 'r')
     if PART == 1:
-        part1()
-    elif PART == 2:
-        part2()
+        out = part1(file)
     else:
-        exit(1)
+        out = part2(file)
+    print(out)
 
