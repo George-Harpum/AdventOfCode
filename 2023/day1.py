6212ee84aa6df38bdd7a8837f89d6ba7565d2236
George-Harpum <157149642+George-Harpum@users.noreply.github.com>
2024-01-27 08:31:00 +0000

    Fixed non-overlapping search bug

--- 2023/day1.py
+++ 2023/day1.py
@@ -10,32 +10,33 @@ FILE_LOC = "./inputs/day1.txt"
 import re
 
 def part1(content):
-    digits = []
+    total = 0
     pattern = re.compile("[0-9]")
     for line in content:
         res = pattern.findall(line)
-        digits += [int(res[0]+res[-1])]
-    return sum(digits)
+        total += int(res[0]+res[-1])
+    return total
 
 def part2(content):
-    digits = []
+    total = 0
     num_as_word = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    num_dict = dict(zip(num_as_word, [str(x) for x in range(0, 10)]))
-    pattern = re.compile("|".join(num_as_word)+"|[0-9]")
+    num_dict = dict(zip(num_as_word, [str(x) for x in range(1, 10)]))
+    pattern = re.compile(f"(?=({'|'.join(num_as_word)+'|[0-9]'}))")
     for line in content:
         find = pattern.findall(line)
         res = list(map(lambda x: num_dict.get(x, x), find))
-        digits += [int(res[0]+res[-1])]
-    return sum(digits)
+        total += int(res[0]+res[-1])
+    return total
 
 if __name__ == "__main__":
-    PART = input("Which part?")
-    if PART not in ('1', '2'):
+    part = input("Which part?")
+    if part not in ('1', '2'):
         raise ValueError("Part must be 1 or 2")
     file = open(FILE_LOC, 'r')
-    if PART == '1':
+    if part == '1':
         out = part1(file)
     else:
         out = part2(file)
+    file.close()
     print(out)
 
