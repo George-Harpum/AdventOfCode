c914d95a87c581279c94e7895033e2a090967bef
George-Harpum <157149642+George-Harpum@users.noreply.github.com>
2024-01-28 08:10:01 +0000

    day2 part1 implementation

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,4 +1,71 @@
 """
 Day2
+Problem statement
+
+A bag contains red, blue, and green cubes. 
+A game consists of several random selections of cubes are taken from the bag (with replacement)
+Each game has a new bag with variable number of cubes.
+
 Part 1
+Which games are possible provided a bag contains exactly 12 red, 13 green, and 14 blue cubes?
+
+Part 2
+What are the minimum number of each colour cube for the game to be possible
+Multiply those numbers together and sum that result for each game
+e.g. a game requiring 2 red, 3 blue, and 4 green has a 'power' of 24. 
 """
+
+FILE_LOC = ".\inputs\day2.txt"
+
+
+def test_max(string, red_max, green_max, blue_max):
+    split = string.split(' ')
+    num = int(split[0])
+    match split[1].strip('\n'):
+        case 'blue':
+            if num > blue_max:
+                return False
+        case 'red':
+            if num > red_max:
+                return False
+        case 'green':
+            if num > green_max:
+                return False
+    return True
+
+def part1(content, rMax=12, gMax=13, bMax=14):
+    total = 0
+    for line in content:
+        game_parts = line.split(': ')
+        game_id = int(game_parts[0][5:])
+        selections = game_parts[1].split('; ')
+        tag = True
+        for s in selections:
+            colours = s.split(', ')
+            if all([test_max(c, rMax, gMax, bMax) for c in colours]) is False:
+                tag = False
+                break
+        if tag is True:
+            total += game_id
+    return total
+
+
+def part2():
+    raise NotImplementedError
+
+
+def main():
+    part = input("Which part?")
+    if part not in '12':
+        raise ValueError("Part must be 1 or 2")
+    file = open(FILE_LOC, 'r')
+    if part == '1':
+        out = part1(file)
+    else:
+        out = part2(file)
+    file.close()
+    print(out)
+
+if __name__ == "__main__":
+    main()
+
